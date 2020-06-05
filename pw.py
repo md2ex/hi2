6b17586c079e5c3b49803330db6c4a12222c0728
min <x58732201@gmail.com>
2020-06-05 19:59:49 +0800

    pw_v1.1

--- pw.py
+++ pw.py
@@ -1,8 +1,11 @@
 x = 3
 while x > 0:
+	x = x - 1
 	password = input('請輸入密碼')
 	if password != ('a123456'):
-		x = x - 1
+		if x == 0:
+			print('"密碼錯誤!')
+			break
 		print('"密碼錯誤! 還有', x, '次機會"')
 	else:
 		print("登入成功")
