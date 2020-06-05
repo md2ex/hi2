3eca6a4696c0898969a91a16d9b4af8e6bbc0663
min <x58732201@gmail.com>
2020-06-05 21:25:56 +0800

    num_v1.1

--- num.py
+++ num.py
@@ -1,13 +1,17 @@
 import random
 x = random.randint(1, 100)
+count = 0
 while True:
 	y = input('請猜數字1-100: ')
 	y = int(y)
+	count = count + 1
 	if y == x:
 		print('"終於猜對了"')
+		print('這是你猜的第', count, '次')	
 		break
-	elif y > x:
+	elif y > x:		
 		print('比答案大')
-	else:
-		print('比答案小')	
+	elif y < x:		
+		print('比答案小')
+	print('這是你猜的第', count, '次')	
 
